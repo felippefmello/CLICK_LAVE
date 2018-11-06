9793a54ef08bb61f48b785441831439a51a3431d
Felippe <felippe@meubiz.com.br>
2018-11-06 12:59:38 -0200

    adicionar routes e controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
+  get 'orders/new'
+  get 'laundries/new'
+  get 'laundries/edit'
+  get 'laundries/index'
+  get 'laundries/show'
   devise_for :users
   root to: 'pages#home'
+  resources :laundries do
+    resources :orders, only: [:new, :create]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
