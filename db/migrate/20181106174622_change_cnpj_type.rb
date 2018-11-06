50b181343894069a9e215fc2c26c9a63f83135b1
Livia Gratz <livia.gratz@gmail.com>
2018-11-06 18:03:24 -0200

    Testing controllers

--- db/migrate/20181106174622_change_cnpj_type.rb
+++ db/migrate/20181106174622_change_cnpj_type.rb
@@ -1,5 +1,3 @@
 class ChangeCnpjType < ActiveRecord::Migration[5.2]
-  def change
-    change_column :laundries, :cnpj, :integer
-  end
+
 end
--- db/migrate/20181106174742_change_type_cnpj.rb
+++ db/migrate/20181106174742_change_type_cnpj.rb
@@ -1,5 +1,3 @@
 class ChangeTypeCnpj < ActiveRecord::Migration[5.2]
-  def change
-    change_column :laundries, cnpj: :integer
-  end
+
 end
