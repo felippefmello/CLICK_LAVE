421c2d39ffede398c9552fa4b75da6d956439cf4
Livia Gratz <livia.gratz@gmail.com>
2018-11-08 16:43:31 -0200

    Add controllers for orders

--- app/controllers/laundry/orders_controller.rb
+++ app/controllers/laundry/orders_controller.rb
@@ -1,5 +1,5 @@
 class Laundry::OrdersController < ApplicationController
   def index
-    @orders = Order.where(laundry: current_user.laundries).all
+    @orders = Order.where(laundry: current_user.laundries).all.sort_by{|order| order.status ? 1 : 0}
   end
 end
