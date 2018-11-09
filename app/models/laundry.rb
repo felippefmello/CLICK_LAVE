78b46835fe1383bdff322dcfe517abd0bbd920e4
Livia Gratz <livia.gratz@gmail.com>
2018-11-09 10:27:24 -0200

    Melhorando actions das ordens

--- app/models/laundry.rb
+++ app/models/laundry.rb
@@ -2,8 +2,8 @@ class Laundry < ApplicationRecord
   belongs_to :user
   has_many :orders, dependent: :destroy
 
-  geocoded_by :address
-  after_validation :geocode, if: :will_save_change_to_address?
+  # geocoded_by :address
+  # after_validation :geocode, if: :will_save_change_to_address?
 
   validates :name, presence: true, uniqueness: true
   validates :address, presence: true
